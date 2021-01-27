2e83659f7279c9c3f3d345762bb28cc3ba26a1ca
oneavk <oneavk@gmail.com>
2021-01-27 23:04:05 +0700

    Added models Company and Employee to admin site.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from api.models import Company, Employee
 
-# Register your models here.
+
+class ApiCompanyAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    fields = ('id', 'name')
+
+
+class ApiEmployeeAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    fields = ('id', 'last_name', 'first_name', 'middle_name', 'company')
+
+
+admin.site.register(Company, ApiCompanyAdmin)
+admin.site.register(Employee, ApiEmployeeAdmin)
