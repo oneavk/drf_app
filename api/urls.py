c1b44cc86cdfb1e96d8d8edffb9b7579d56658d6
oneavk <oneavk@gmail.com>
2021-01-28 22:09:49 +0700

    Updated urlpatterns for companies

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from api.views.company import CompanyViewSet
+
+router = DefaultRouter(trailing_slash=True)
+router.register('companies', CompanyViewSet, 'company')
+
+urlpatterns = router.urls
