c2258ff9fe374adae6df4c36d5c92ac8549d9560
oneavk <oneavk@gmail.com>
2021-01-28 22:07:34 +0700

    Added company ViewSet

---
+++ api/views/company.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from api.serializers.company import CompanySerializer
+from api.models import Company
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
