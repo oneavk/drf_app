7efb1af7ac1dd5733378fb679c5cd2913c388483
oneavk <oneavk@gmail.com>
2021-02-04 22:28:47 +0700

    Added employee create, update and delete tests

--- api/tests/employee.py
+++ api/tests/employee.py
@@ -7,33 +7,70 @@ from api.models import Employee
 
 class EmployeeTestCase(APITestCase):
     def setUp(self) -> None:
-        company = create_company(company_list[0]['name'])
-        for i in range(len(employee_list)):
-            create_employee(
-                employee_list[i]['last_name'],
-                employee_list[i]['first_name'],
-                employee_list[i]['middle_name'],
-                company
-            )
-            employee_list[i]['company'] = company.id
+        self.company = create_company(company_list[0]['name'])
+        for employee in employee_list:
+            employee['company'] = self.company.id
+
+        self.employee = create_employee(
+            employee_list[0]['last_name'],
+            employee_list[0]['first_name'],
+            employee_list[0]['middle_name'],
+            self.company
+        )
 
     def test_list(self) -> None:
         get_auth(self.client)
+        for i in range(1, len(employee_list)):
+            create_employee(
+                    employee_list[i]['last_name'],
+                    employee_list[i]['first_name'],
+                    employee_list[i]['middle_name'],
+                    self.company
+                )
         response = self.client.get('/api/employees/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, employee_list)
 
     def test_retrieve(self) -> None:
         get_auth(self.client)
-        response = self.client.get('/api/employees/1/')
+        response = self.client.get(f'/api/employees/{self.employee.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, employee_list[0])
 
+    def test_create(self) -> None:
+        get_auth(self.client)
+        response = self.client.post('/api/employees/', employee_list[2])
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        employee = Employee.objects.get(pk=response.data['id'])
+        self.assertEqual(employee.name, employee_list[2]['name'])
+
+    def test_update(self) -> None:
+        get_auth(self.client)
+        employee_data = employee_list[0].copy()
+        employee_data['first_name'] = 'updated_name'
+
+        response = self.client.put(f'/api/employees/{self.employee.id}/', employee_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        employee = Employee.objects.get(pk=self.employee.id)
+        self.assertEqual(employee.last_name, employee_data['last_name'])
+        self.assertEqual(employee.first_name, employee_data['first_name'])
+        self.assertEqual(employee.middle_name, employee_data['middle_name'])
+
+    def test_destroy(self) -> None:
+        get_auth(self.client)
+        response = self.client.delete(f'/api/employees/{self.employee.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Employee.DoesNotExist):
+            Employee.objects.get(pk=self.employee.id)
+
     def test_name_prop(self) -> None:
-        empl = Employee()
-        empl.last_name = 'last'
-        empl.first_name = 'first'
-        self.assertEqual(empl.name, 'last first')
+        employee = Employee()
+        employee.last_name = 'last'
+        employee.first_name = 'first'
+        self.assertEqual(employee.name, 'last first')
 
     def test_list_without_auth(self) -> None:
         response = self.client.get('/api/employees/')
@@ -41,4 +78,16 @@ class EmployeeTestCase(APITestCase):
 
     def test_retrieve_without_auth(self) -> None:
         response = self.client.get('/api/employees/1/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_without_auth(self) -> None:
+        response = self.client.post('/api/employees/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_without_auth(self) -> None:
+        response = self.client.put('/api/employees/1/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_destroy_without_auth(self) -> None:
+        response = self.client.delete('/api/employees/1/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
